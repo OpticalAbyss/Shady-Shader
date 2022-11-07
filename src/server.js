085725d152ab8e254dc638e73db123926d805a52
Obyss <khanumair2345@gmail.com>
2022-11-07 08:29:59 +0500

    Removed Additional Pages, will use a single dynamic page, cleaned up the page for a cleaner look

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ const fs = require(`fs`);
 const path = require(`path`);
 const port = 8000;
 
-const about = fs.readFileSync(path.join(__dirname, `../public/about.html`), "utf-8");
 const home = fs.readFileSync(path.join(__dirname, `../public/home.html`), "utf-8");
-const contact = fs.readFileSync(path.join(__dirname, `../public/contact.html`), "utf-8");
 
 const server = http.createServer((req, res)=>{
   if(req.url == `/`)
@@ -15,30 +13,12 @@ const server = http.createServer((req, res)=>{
       res.write(home + `<style>${data}</style>`)
       res.end()
     })
-  }
-  else if(req.url == `/about`)
-  {
-    res.writeHead(200, {'Content-Type' : `text/html`})
-    fs.readFile(path.join(__dirname, `../public/css/style.css`), "utf-8", (err,data)=>{
-      res.write(about + `<style>${data}</style>`)
-      res.end()
-    })
-  }
-  else if(req.url == `/contact`)
-  {
-    res.writeHead(200, {'Content-Type' : `text/html`})
-    fs.readFile(path.join(__dirname, `../public/css/style.css`), "utf-8", (err,data)=>{
-      res.write(contact + `<style>${data}</style>`)
-      res.end()
-    })
-  }
+  }// Deccided to make a single page
   else {
     res.end(`404 Page not Found`)
   }
 
 });
-
-
 
 server.listen(port, (err)=>{
   if(err) console.log(err);
